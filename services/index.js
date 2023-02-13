b020ca54d888fbc918dbd2366a9a7a37dc2c677d
silje <angelviksilje@gmail.com>
2023-02-13 17:14:03 +0100

    Comments Form is now successfully submitting comments

--- services/index.js
+++ services/index.js
@@ -65,7 +65,7 @@ export const getPostsDetails = async (slug) => {
     }
  }
 `
-    const result = await request(graphqlAPI, query, { slug });
+    const result = await request(graphqlAPI, query, {slug});
     return result.post;
 }
 
@@ -121,4 +121,16 @@ export const getCategories = async () => {
 `
     const result = await request(graphqlAPI, query);
     return result.categories;
+}
+
+export const submitComment = async (obj) => {
+    const result = await fetch('/api/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    });
+
+    return result.json();
 }
