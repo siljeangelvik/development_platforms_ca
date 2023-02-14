fdddd51675fac9a58665c0555cf1d87ce2a6db57
silje <angelviksilje@gmail.com>
2023-02-14 11:23:22 +0100

    tried to add the belonging categories to the post

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -76,6 +76,7 @@ const PostDetail = ({post}) => {
                     return getContentFragment(index, children, typeObj, typeObj.type)
                 })}
             </div>
+            <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.categories.name}</p>
         </div>
     )
 }
